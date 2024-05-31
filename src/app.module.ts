5897fecb9097e6531f00bbf3afd5e65502ce6783
Daniel Parada <79163757+danielpcode2@users.noreply.github.com>
2024-05-31 09:54:42 -0300

    add module restaurant

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { RestaurantModule } from './restaurant/restaurant.module';
+import { HealthModule } from './health/health.module';
+import { DynamoClientsModule } from './dynamodb/dynamodb.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HealthModule,
+    RestaurantModule,
+    DynamoClientsModule,
+  ],
+  controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
   const config = new DocumentBuilder()
     .setTitle('Restaurant Management')
     .setDescription('Restaurant management api description')
     .setVersion('1.0')
-    .addTag('Restaurant')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doocuments', app, document);
+  SwaggerModule.setup('document', app, document);
 
   await app.listen(8080);
+  Logger.log(`Application backend is running on: ${await app.getUrl()}`);
 }
 bootstrap();
